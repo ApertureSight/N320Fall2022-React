cb485b9880f768951dc0a696666cca7138259ccc
ApertureSight <97801261+ApertureSight@users.noreply.github.com>
2022-09-07 17:57:04 -0400

    Next steps...

--- Labs/Musical Inheritance/app.js
+++ Labs/Musical Inheritance/app.js
@@ -3,21 +3,29 @@ let family = ['Reed', 'Percussion', 'woodwind', 'string', 'Air'];
 class Instrument {
     constructor(volume, family, playing){
     this.volume = volume;
-    this.family = family;
+    this.family;
     this.playing = playing;
+    
 
-
-    }}
+    }
 // instrument method
 
-var myInstrument = new Instrument(5, 1, 10);
+// var myInstrument = new Instrument(5, 1, 10);
 
-
-function anInstrument(Instrument){
-   
-    console.log('You are listening to a(n) ${this.family} for ${this.playing} at a volume of ${this.volume}');
+class Woodwind extends Instrument{
+    constructor(volume, family, playing){
+        super(volume, family, playing);
+        this.kind = "Woodwind";
+    }
+    plays(){
+        console.log("You are listening to an instrument from the "+this.family );
+    }
 }
+//  playInstrument(){
+     
+//     console.log('You are listening to a(n) ${this.family} for ${this.playing} at a volume of ${this.volume}');
+// }
+// }
 
-
-
-console.log(myInstrument);
+// var myInstrument = new Instrument(5, 1, 10);
+// console.log(myInstrument);
