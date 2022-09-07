f0432689ec04c7029a688f71db636ebcce46a55f
ApertureSight <97801261+ApertureSight@users.noreply.github.com>
2022-09-07 17:30:47 -0400

    Sort of works. Not passing values from array

--- Labs/Musical Inheritance/app.js
+++ Labs/Musical Inheritance/app.js
@@ -1,16 +1,23 @@
+let family = ['Reed', 'Percussion', 'woodwind', 'string', 'Air'];
+
 class Instrument {
     constructor(volume, family, playing){
     this.volume = volume;
     this.family = family;
     this.playing = playing;
 
-    }
+
+    }}
 // instrument method
 
-listening(duration){
-    this.duration = duration;
+var myInstrument = new Instrument(5, 1, 10);
 
-    return 'You are listening to a(n) ${this.family} for ${this.playing} at a volume of ${this.volume} for ${this.duration}'
-}
+
+function anInstrument(Instrument){
+   
+    console.log('You are listening to a(n) ${this.family} for ${this.playing} at a volume of ${this.volume}');
 }
 
+
+
+console.log(myInstrument);
