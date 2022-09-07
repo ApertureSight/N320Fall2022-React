efb59d3ddede3741c1690c32ef5e128a1a92dade
ApertureSight <97801261+ApertureSight@users.noreply.github.com>
2022-09-07 16:45:01 -0400

    Musican Inheritance

--- Labs/Musical Inheritance/app.js
+++ Labs/Musical Inheritance/app.js
@@ -1,2 +1,16 @@
-<script src="https://https://cdnjs.cloudflare.com/ajax/libs/tone/14.8.42/Tone.js.com/tone"></script>
+class Instrument {
+    constructor(volume, family, playing){
+    this.volume = volume;
+    this.family = family;
+    this.playing = playing;
+
+    }
+// instrument method
+
+listening(duration){
+    this.duration = duration;
+
+    return 'You are listening to a(n) ${this.family} for ${this.playing} at a volume of ${this.volume} for ${this.duration}'
+}
+}
 
