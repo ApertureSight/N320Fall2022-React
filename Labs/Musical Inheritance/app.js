f0d6d74607cb9f1ff91450d050dd672f6ee4e936
ApertureSight <97801261+ApertureSight@users.noreply.github.com>
2022-09-08 12:28:22 -0400

    Version 3, Update to requirements using professor example

--- Labs/Musical Inheritance/app.js
+++ Labs/Musical Inheritance/app.js
@@ -3,28 +3,57 @@
 class Instrument {
     constructor(volume, family, playing){
     this.volume = volume;
-    this.family;
+    this.family = family;
     this.playing = playing;
     
 
  }
-// instrument method
-
-
-// class Woodwind extends Instrument{
-//     constructor(volume, family, playing){
-//         super(volume, family, playing);
-//         this.kind = "woodwind";
-//     }
-//     plays(){
-//         console.log("You are listening to an instrument from the "+this.family );
-//     }
-
- playInstrument(){
-     
-    console.log('You are listening to a(n) ${this.family} for ${this.playing} at a volume of ${this.volume}');
+    displayDetails(){
+        console.log("This Instrument is usually played at a " +this.volume +" volume, it is a member of the " + this.family +" family and is in this song for approximately " + this.playing);
+    }
 }
+class Guitar extends Instrument {
+    constructor(make, strings, type){
+          super();
+          this.family = super.family;
+        this.make = make;
+        this.strings = strings;
+        this.type = type;
+
+    }
+    strumOpen(){
+        console.log(this.strings);
+        this.strings.forEach(strings => {
+            console.log(strings.note + " Playing")
+        });
+    }
+    displayDetails(){
+        
+        
+        console.log("This Guitar is made by " +this.make +" and it is a(n) " +this.type + " guitar and it has " + this.strings.length +" strings");
+    }
 }
 
-var myInstrument = new Instrument(5, 1, 10);
-console.log(myInstrument);
+class String {
+    constructor(note, octave){
+        this.note = note;
+        this.octave = octave;
+
+    }
+}
+
+var strings = [];
+strings[0]  = new String ("E", 4);
+strings[1]  = new String ("A", 4);
+strings[2]  = new String ("D", 4);
+strings[3]  = new String ("G", 4);
+strings[4]  = new String ("B", 4);
+strings[5]  = new String ("E", 4);
+
+var myInstrument = new Instrument("Moderate", "String", "3 minutes");
+var myGuitar = new Guitar ("Taylor", strings, "Acoustic");
+
+
+myGuitar.strumOpen();
+myGuitar.displayDetails();
+myInstrument.displayDetails();
